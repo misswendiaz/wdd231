179a6034b3e385a41d16c7c7c270b10d776cf778
misswendiaz <ldiaz@byupathway.edu>
2025-09-10 22:15:49 +0800

    prophets

---
+++ week02/scripts/json.js
@@ -0,0 +1,24 @@
+{
+    "familyName" : "Diaz",
+        "move_in_date" : "2024-02-18",
+            "number_of_people": 1,
+                "visited_by_bishopric": false,
+                    "family_members": [
+                        {
+                            "name": "Aloysius Emmanuel Hernandez Diaz",
+                            "gender": "Male",
+                            "birthdate": "1978-12-20"
+                        },
+                        {
+                            "name": "Laureen Capistrano Arcilla Diaz",
+                            "gender": "Female",
+                            "birthdate": "1986-09-12"
+                        },
+                        {
+                            "name": "Emmanuel Aloysius Arcilla Diaz",
+                            "gender": "Male",
+                            "birthdate": "2011-02-25"
+                        }
+                    ]
+
+}
