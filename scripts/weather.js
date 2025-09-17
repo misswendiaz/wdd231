b5011699b29135298dd207a4c53515b2b0445021
misswendiaz <ldiaz@byupathway.edu>
2025-09-17 20:28:27 +0800

    weather

---
+++ scripts/weather.js
@@ -0,0 +1,22 @@
+const currentTemp = document.querySelector("#current-temp");
+const weatherIcon = document.querySelector("#weather-icon");
+const captionDesc = document.querySelector("figcaption");
+
+const url = "https://api.openweathermap.org/data/3.0/onecall?lat=49.75&lon=6.64&unit=metric&appid=5e4c6f2a32f9b99cf16e440f92f86479";
+
+async function apiFetch() {
+    try {
+        const response = await fetch(url);
+        if (response.ok) {
+            const data = await response.json();
+            console.log(data); // testing only
+            // displayResults(data); // uncomment when ready
+        } else {
+            throw Error(await response.text());
+        }
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+apiFetch();
