8cf02e762ca40424be966e34f394cfd4f5ca7e65
misswendiaz <ldiaz@byupathway.edu>
2025-09-18 12:59:18 +0800

    api

--- week03/scripts/weather.js
+++ week03/scripts/weather.js
@@ -8,7 +8,7 @@ const weatherIcon = document.querySelector("#weather-icon");
 const captionDesc = document.querySelector("figcaption");
 
 // Create required variables for the url
-const myKey = "58dd665c70079ca117440af054f91ca4"
+const myKey = "1e08c4fc57a3294e16414fd0a18d8f33"
 const myLat = "49.75"
 const myLong = "6.64"
 
@@ -21,7 +21,7 @@ async function apiFetch() {
         const response = await fetch(myUrl);
         if (response.ok) {
             const data = await response.json();
-            // console.log(data); // testing only
+            console.log(data); // testing only
             displayResults(data); // uncomment when ready
         } else {
             throw Error(await response.text());
@@ -33,11 +33,13 @@ async function apiFetch() {
 
 // Display the JSON data onto my web page
 function displayResults(data) {
-    // console.log("hello")
+    // console.log(data.weather[0].icon);
     myTown.innerHTML = data.name
     myDescription.innerHTML = data.weather[0].description
     myTemperature.innerHTML = `${data.main.temp}&deg;C`
+    console.log("Icon code:", data.weather[0].icon);
     const iconsrc = `https://openweathermap.org/img/wn/${data.weather[0].icon}@2x.png`
+    console.log("Icon URL:", iconsrc);
     myGraphic.setAttribute("src", iconsrc)
     myGraphic.setAttribute("alt", data.weather[0].description)
     currentTemp.textContent = `${data.main.temp} °C`;
