091c80ddae47750bffc47c2fcd7fe53718617015
misswendiaz <ldiaz@byupathway.edu>
2025-09-27 12:56:48 +0800

    ty data

--- chamber/scripts/thankyou.mjs
+++ chamber/scripts/thankyou.mjs
@@ -4,8 +4,11 @@ document.addEventListener("DOMContentLoaded", () => {
 
     document.getElementById("firstName").textContent = params.get("First_Name") || "Not provided";
     document.getElementById("lastName").textContent = params.get("Last_Name") || "Not provided";
+    document.getElementById("organizationalTitle").textContent = params.get("Title") || "Not provided";
     document.getElementById("email").textContent = params.get("Email") || "Not provided";
     document.getElementById("phone").textContent = params.get("Phone") || "Not provided";
     document.getElementById("business").textContent = params.get("Organization") || "Not provided";
-    document.getElementById("timestamp").textContent = params.get("Time_Stamp") || new Date().toLocaleString();
+    document.getElementById("description").textContent = params.get("Description") || "Not provided";
+    document.getElementById("membershipLevel").textContent = params.get("Membership") || "Not provided";
+    document.getElementById("timeStamp").textContent = params.get("Time_Stamp") || new Date().toLocaleString();
 });
